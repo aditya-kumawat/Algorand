1e0851ca0c576974e0a58f11fbc7d20a1f6fe0c6
root <root@localhost.localdomain>
2018-11-21 01:52:32 +0000

    Basic working

--- client.js
+++ client.js
@@ -1,1 +1,20 @@
 var socket = io();
+
+var data = {};
+
+socket.on('connect', () => {
+	socket.emit('fetchData', (value) => {
+		data = value;
+		if(data.j==1) {
+			document.body.style.backgroundColor = "green";
+		} else {
+			document.body.style.backgroundColor = "red";
+		}
+	});
+})
+
+function verify() {
+	socket.emit('verify', data.publicKey, data.value, data.proof, (value) => {
+		console.log(value);
+	})
+}
--- server.js
+++ server.js
@@ -12,37 +12,51 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
+	socket.on('fetchData', (cb) => {
+		cb(fetchData());
+	})
 
+	socket.on('verify', (publicKey, value, proof, cb) => {
+		cb(verify(publicKey, value, proof));
+	})
 });
-
 
 const {utils, ecvrf, sortition} = require('vrf.js')
 const seed = Buffer.from('sortition')
 const role = Buffer.from('test')
 const w = utils.N(1)
-const W = utils.N(5)
-const tau = utils.N(1)
+const W = utils.N(100)
+const tau = utils.N(10)
 
+function fetchData() {
+	const [publicKey, privateKey] = utils.generatePair()
+	const [value, proof, j] = sortition.sortition(privateKey, publicKey, seed, tau, role, w, W);
 
-// const [publicKey, privateKey] = utils.generatePair()
-// const [value, proof, j] = sortition.sortition(
-//   privateKey, publicKey,
-//   seed, tau, role, w, W
-// )
+	return {
+		publicKey: publicKey,
+		privateKey: privateKey,
+		seed: seed,
+		role: role,
+		w: w, 
+		W: W,
+		tau: tau,
+		value: value,
+		proof: proof,
+		j: j
+	}
+}
 
-// if (+j > 0) {
-//   // next
-// }
+function verify(publicKey, value, proof) {
+	return sortition.verifySort(publicKey, value, proof, seed, tau, role, w, W);
+}
 
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 10; i++) {
   const [publicKey, privateKey] = utils.generatePair()
-  const [value, proof, j] = sortition.sortition(privateKey, publicKey, seed, tau, role, w, W);
-  	if(j>0) {
+  const [value, proof, j] = sortition.sortition(privateKey, publicKey, seed, tau, role, w, W)
+  if(j>0) {
 	  console.log(`-------------- test ${i} --------------`)
 	  console.log(' value:', value.toString('hex'))
 	  console.log('     j:', j.toString())
 	  console.log('result:', j.gt(0))
-
-	  console.log(sortition.verifySort(publicKey, value, proof, seed, tau, role, w, W));
 	}
 }
